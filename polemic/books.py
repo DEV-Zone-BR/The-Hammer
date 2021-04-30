2d64d57a495cafd59a2a8f1f1289c7d31d6b8432
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-04-30 00:37:06 -0300

    issue #6 feat: book length limit implemented

--- polemic/books.py
+++ polemic/books.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 async def book_search(ctx: discord, book:str):
   
   try:
-    response = requests.get(f'https://plmcbks.amanoteam.com/search/books?query_name={book}&search_type=fast&page_number=0&max_items=10')
+    response = requests.get(f'https://plmcbks.amanoteam.com/search/books?query_name={book}&search_type=fast&page_number=0&max_items=5')
 
     print(response.status_code)
 
