c9880e67112c971dcebc0dd370700588e49b3610
Bruno Alexandre Gomes de Souza <souza.bruno.alexandre@gmail.com>
2021-04-30 00:34:09 -0300

    feat: booklist display ready

--- polemic/books.py
+++ polemic/books.py
@@ -7,13 +7,26 @@ from discord.ext import commands
 async def book_search(ctx: discord, book:str):
   
   try:
-    request = requests.get(f'https://plmcbks.amanoteam.com/search/books?query_name={book}&search_type=fast&page_number=0&max_items=1000')
+    response = requests.get(f'https://plmcbks.amanoteam.com/search/books?query_name={book}&search_type=fast&page_number=0&max_items=10')
 
-    print(request.status_code, requests.json())
+    print(response.status_code)
 
-    if(request.status_code==200):
-      pass
+    if(response.status_code==200):
+
+      print("here")
+
+      await ctx.send(f'Olá, Aqui está sua lista de livros polêmicos')
+
+      books = response.json()["results"]["items"]      
+
+      for book in books:
+
+        await ctx.send(f'```Título: {book["title"]}\nCategoria:{book["category"]["name"]}\nAuthor:{book["author"]["name"]}```\nLink Para Download: https://plmcbks.amanoteam.com/view/{book["id"]}')        
+
+      # https://plmcbks.amanoteam.com/view/13637
+    
     else:
       pass
-  except Exception:
-    pass
+  except Exception as e:
+    print(e)
+    print(response.status_code)
