53aeca0908227fb4040efe09f2970450c2c9f973
Luiz Matheus <luizmatheusga@gmail.com>
2021-05-19 22:30:23 -0300

    feat: add check_member to verify the user permission

--- members/manager.py
+++ members/manager.py
@@ -1,24 +1,34 @@
 import discord
 from discord.ext import commands
 
-from config import ETERNAL_ROLES
+from config import ETERNAL_ROLES, ALLOWED_ROLES
+from authorization import check_member
 
 
 @commands.command()
 async def joined(ctx: discord, member: discord.Member):
     """Says when a member joined."""
     print(member)
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+    await ctx.send("{0.name} joined in {0.joined_at}".format(member))
 
 
 @commands.command()
 async def ban(ctx: discord, member: discord.Member):
-    channel = [c for c in ctx.guild.channels if c.name.endswith('__geral')][0] or None
+    if not await check_member(ctx, ALLOWED_ROLES["ban"]):
+        return
+
+    channel = [c for c in ctx.guild.channels if c.name.endswith("__geral")][0] or None
     roles = set([r.name for r in member.roles]) & ETERNAL_ROLES
     print(roles)
     if len(roles) > 0:
-        await ctx.send('Você não pode remover um membro do conselho', )
+        await ctx.send(
+            "Você não pode remover um membro do conselho",
+        )
     else:
-        await channel.send(':hammer: {0.name} estava muito tempo sem dar um oi para os amigos'.format(member))
-        await ctx.send('{0.name} foi removido por inatividade'.format(member))
+        await channel.send(
+            ":hammer: {0.name} estava muito tempo sem dar um oi para os amigos".format(
+                member
+            )
+        )
+        await ctx.send("{0.name} foi removido por inatividade".format(member))
         await ctx.guild.kick(member)
