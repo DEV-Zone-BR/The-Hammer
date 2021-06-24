a025f2a4f9ee9922a8d849b194e5a918b0650b2d
felipe <felipematheusmellodemorais@gmail.com>
2021-06-23 23:22:04 -0300

    feat: stonks command was added

--- members/__init__.py
+++ members/__init__.py
@@ -1,10 +1,11 @@
 from discord.ext.commands import Bot
 
-from members.manager import ban, joined
+from members.manager import ban, joined, stonks
 
 
 def init(bot: Bot):
     bot.add_command(ban)
     bot.add_command(joined)
+    bot.add_command(stonks)
 
     return bot
---
+++ stonks/types.py
@@ -0,0 +1,8 @@
+from typing import TypedDict
+
+
+class StonksResult(TypedDict):
+    name: str
+    price: str
+    div_yield: str
+    equity_value: str
